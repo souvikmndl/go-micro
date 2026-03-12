d28881a5f8b3316245368f6bab85a682663b2c11
souvikmndl <svk72mi@gmail.com>
2026-03-12 19:17:07 +0530

    setting up frontend and broker

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("starting broker service on port %s\n", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panicf("error starting broker server %+v\n", err)
+	}
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,9 @@
+module github.com/souvikmndl/broker-service
+
+go 1.24.2
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/go-chi/cors v1.2.2
+)
